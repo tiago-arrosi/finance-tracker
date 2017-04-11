22b93f97676f447c2bc16d03cf133afff03c1897
Tiago Arrosi <twarrosi@gmail.com>
2017-04-11 20:07:10 -0300

    add user ans stock association

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
   devise_for :users
+  resources :user_stocks, except: [:show, :edit, :update]
+
   root 'welcome#index'
-
   get 'my_portfolio', to:"users#my_portfolio"
-
   get 'search_stocks', to: "stocks#search"
 
   # The priority is based upon order of creation: first created -> highest priority.
